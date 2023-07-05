4b37698bc204ee95892140751105ea94ec186cf1
jianmosier <875816119@qq.com>
2023-07-05 10:34:51 +0800

    86 half pass

--- 1Array/86.分隔链表.cpp
+++ 1Array/86.分隔链表.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<stdio.h>
+#include<vector>
 
 using namespace std;
 
@@ -13,34 +14,40 @@ struct ListNode {
 
 class Solution {
 public:
-
     ListNode* partition(ListNode* head, int x) {
-        ListNode* varNode, *constNode, *largeNode, *largeHead, *conectNode;
-        varNode->next = head;
-        constNode = varNode;
-        while(varNode->next != nullptr && varNode->val < x){
-            varNode = varNode->next;     
-        }
-        if(varNode->val >=  x){
-            conectNode = varNode;
-            largeNode->next = varNode;
-            largeHead = largeNode;
-            while(varNode->next != nullptr){
-                if( varNode->val < x){
-                    varNode = varNode->next;    
-                }
-                else{
-                    largeNode->next = varNode;
-                }
+        ListNode lessHead, moreHead;
+        ListNode *lessPtr, *morePtr;
+        lessPtr = &lessHead;
+        morePtr = &moreHead;
+        while(head){
+            if(head->val < x){
+                lessPtr->next = head;
+                lessPtr = head;
             }
+            else{
+                morePtr->next = head;
+                morePtr = head;
+            }
+            head->next = head;
         }
-        else{
-            return constNode->next;
-        }
-        conectNode->next = largeHead->next;
-        return constNode->next;
+        lessPtr->next = moreHead.next;
+        return lessHead.next;
     }
 };
 
 int main(){
+    Solution s;
+    vector<int> nums = {1, 4, 3, 2, 5, 2};
+    ListNode* head = new ListNode(nums[0]);
+    ListNode* ptr = head;
+    for(int i = 1; i < nums.size(); i++){
+        ptr->next = new ListNode(nums[i]);
+        ptr = ptr->next;
+    }
+    ListNode* res = s.partition(head, 3);
+    while(res){
+        cout << res->val << " ";
+        res = res->next;
+    }
+    return 0;
 }
