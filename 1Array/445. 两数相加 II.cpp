836dd654ea04468d4bd2cfbdaa7d3b20f203f6ca
jianmosier <875816119@qq.com>
2023-07-12 09:36:37 +0800

    445 self passed

--- 1Array/445. 两数相加 II.cpp
+++ 1Array/445. 两数相加 II.cpp
@@ -12,32 +12,75 @@ struct ListNode{
     ListNode(int val,ListNode* next): val(val), next(next){}
 };
 
-ListNode* reverse(ListNode* head) {
-    ListNode* rightNode = nullptr;
-    while(head) {
-        ListNode* tempNode = head->next;
-        head->next = rightNode;
-        rightNode = head;
-        head = tempNode;
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){
+        ListNode* rightNode = nullptr;
+        while(head){
+            ListNode* tempNode = head->next;
+            head->next = rightNode;
+            rightNode = head;
+            head = tempNode;
+        }
+        return rightNode;
     }
-    return rightNode;
-}
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* rl1 = reverseList(l1);
+        ListNode* rl2 = reverseList(l2);
+        ListNode dumpHead;
+        ListNode* ptr = &dumpHead;
+        int sum = 0;
+        while(rl1 && rl2) {
+            sum += rl1->val + rl2->val;
+            ptr->next = new ListNode(sum % 10);
+            rl1 = rl1->next;
+            rl2 = rl2->next;
+            ptr = ptr->next;
+            sum /= 10;
+        }
+        while(rl1){
+            sum += rl1->val;
+            ptr->next = new ListNode(sum % 10);
+            rl1 = rl1->next;
+            ptr = ptr->next;
+            sum /= 10;
+        }
+        while(rl2){
+            sum += rl2->val;
+            ptr->next = new ListNode(sum % 10);
+            rl2 = rl2->next;
+            ptr = ptr->next;
+            sum /= 10;
+        }
+        ptr->next = sum ? new ListNode(sum) : nullptr;
+        return reverseList(dumpHead.next);
+    }
+};
 
 int main(){
-    vector<int> nums = {1,2,3,4,5};
-    ListNode dumpHead;
-    ListNode* ptr = &dumpHead;
+    vector<int> nums1 = {1,2,3,4,5};
+    vector<int> nums2 = {1,2,3,4,5,7,8,9};
+    ListNode dumpHead1, dumpHead2;
+    ListNode* ptr1 = &dumpHead1;
+    ListNode* ptr2 = &dumpHead2;
 
-    for(auto num : nums){
-        ptr->next = new ListNode(num);
-        ptr = ptr->next;
+    for(auto num1 : nums1){
+        ptr1->next = new ListNode(num1);
+        ptr1 = ptr1->next;
     }
 
-    ListNode* reverseList = reverse(dumpHead.next);
+    for(auto num2 : nums2){
+        ptr2->next = new ListNode(num2);
+        ptr2 = ptr2->next;
+    }
 
-    while(reverseList) {
-        cout<<reverseList->val<<' ';
-        reverseList = reverseList->next;
+    Solution s;
+
+    ListNode* sumList = s.addTwoNumbers(dumpHead1.next, dumpHead2.next);
+
+    while(sumList) {
+        cout<<sumList->val<<' ';
+        sumList = sumList->next;
     }
 
     return 0;
