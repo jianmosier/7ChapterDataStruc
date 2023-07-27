6af0087908eafd15f8cf33885157b4fe46c5cdf7
jianmosier <875816119@qq.com>
2023-07-27 18:49:04 +0800

    12 贪心算法的解法

--- 3String/12. 整数转罗马数字.cpp
+++ 3String/12. 整数转罗马数字.cpp
@@ -1,40 +1,23 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 using namespace std;
 
 class Solution {
 public:
-    void toRoman(int base, int& num, char high, char mid, char low, string& s) {
-        int count = num / base;
-        num %= base;
-        if(count >= 9) {
-            s.push_back(low);
-            s.push_back(high);      
-        }
-        else if(count >= 5) {
-            s.push_back(mid);
-            for(int i = count - 5; i > 0; i--) {
-                s.push_back(low);
-            }
-        }
-        else if(count == 4){
-            s.push_back(low);
-            s.push_back(mid);
-        }
-        else {
-            for(int i = count; i > 0; i--) {
-                s.push_back(low);
-            }
-        }
-    }
     string intToRoman(int num) {
-        string s = "";
-        toRoman(1000, num, ' ', ' ', 'M', s);
-        toRoman(100, num, 'M', 'D', 'C', s);
-        toRoman(10, num, 'C', 'L', 'X', s);
-        toRoman(1, num, 'X', 'V', 'I', s);
-        return s;
+        vector<int> values {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> symbols {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        string result;
+        for (int i = 0; i < values.size() && num >= 0; i++) {
+            while (values[i] <= num) {
+                num -= values[i];
+                result += symbols[i];
+            }
+        }
+        return result;
     }
 };
 
